f6cb7dbb29b5273bb60ecc701d8d234ad70fb82f
auburn <auburnete@gmail.com>
2020-03-30 18:18:37 +1000

    add sha256 extractor

--- test/hello.js
+++ test/hello.js
@@ -9,7 +9,7 @@ const readFile_p = Promise.promisify(fs.readFile)
 
 const testRdzip = async (rdzipFilename, expected) => {
   const readIn = await readFile_p(rdzipFilename);
-  const result = await vitals.analyse(readIn);
+  const result = await vitals.analyse(readIn, true);
   return _.isMatch(result, expected);
 }
 
@@ -19,6 +19,7 @@ tap.pass('this is fine');
 
 tap.test('Correct header info from Know You', childTest => {
   return testRdzip("test/Andrew_Huang_-_Know_You.rdzip", {
+    sha256: 'e86fbc334db27ebd59dbc472e38849c871715c787f5d11f30534a05522462340',
     artist: 'Andrew Huang',
     song: 'Know You',
     difficulty: 1,
